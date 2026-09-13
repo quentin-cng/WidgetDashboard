8f013ba59ffe5b348f28ade1fec48b5dbbe65835
Quentin Canaguier <quentin.canaguier@gmail.com>
2026-09-13 12:24:44 -0400

    Added frontend and connected it to backend, styled the widgets

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 from app.routers import users
 from app.routers import dashboard
@@ -6,12 +8,25 @@ from app.routers.auth import router as auth_router
 from app.routers.widgets import weather, github
 #news, stocks, crypto, calendar, tasks
 
+
 app = FastAPI()
 app.include_router(dashboard.router)
 app.include_router(weather.router)
 app.include_router(users.router)
 app.include_router(auth_router)
 app.include_router(github.router)
+
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/login")
+def login_page(request: Request):
+    return templates.TemplateResponse(request=request, name="login.html")
+
+
+@app.get("/dashboard-page")
+def dashboard_page(request: Request):
+    return templates.TemplateResponse(request=request, name="dashboard.html")
 
 
 @app.get("/")
